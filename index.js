96cd85053d355e88e596c5f1704ce97a0af9ecd2
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-04-30 12:01:59 +0200

    Update app.listen to variable port

--- index.js
+++ index.js
@@ -348,8 +348,9 @@ app.use((err, req, res, next) => {
 });
 
 //START SERVER
-app.listen(8080, () => {
-  console.log('Server listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
 });
 
 
