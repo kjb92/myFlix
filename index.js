0c95a59a4be21a97e85ebd4c41ef0e8c71de3a6b
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-05-24 10:14:46 +0200

    Update trusted origins

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //Import CORS
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080']; //enter all trusted origins
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://codesandbox.io/']; //enter all trusted origins
 
 app.use(cors({
   origin: (origin, callback) => {
